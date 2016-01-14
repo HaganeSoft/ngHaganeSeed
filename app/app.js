f3cbd36cff80cd4f8e5c05e0d479901ce719183d
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-01-14 14:24:43 -0600

    initial commit

---
+++ app/app.js
@@ -0,0 +1,1 @@
+var app = angular.module('hgapp', ['ngHagane', 'ngMaterial', 'ngCookies', 'ui.router']);
---
+++ app/app.routes.js
@@ -0,0 +1,14 @@
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/login');
+	$stateProvider
+		.state('login', {
+			url: '/login',
+			templateUrl: 'app/auth/login.html',
+			controller: 'AuthCtlr'
+		})
+		.state('admin', {
+			url: '/admin',
+			templateUrl: 'app/admin/admin.html',
+			controller: 'AdminMainCtlr'
+		});
+});
