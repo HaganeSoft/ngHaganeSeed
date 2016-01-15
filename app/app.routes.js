26510b7d8c9e87477811807ed10324d7816c5c38
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-01-14 19:54:01 -0600

    sistema de autenticacion

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,5 +1,18 @@
+app.run(['$rootScope', '$state', 'hagane', function ($rootScope, $state, hagane) {
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+    var isAuthenticationRequired =  toState.data
+          && toState.data.requiresLogin
+          && !hagane.session.authorize();
+
+    if(isAuthenticationRequired)
+    {
+      event.preventDefault();
+      $state.go('login');
+    }
+	});
+}]);
 app.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/login');
+	$urlRouterProvider.otherwise('login');
 	$stateProvider
 		.state('login', {
 			url: '/login',
@@ -9,6 +22,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
 		.state('admin', {
 			url: '/admin',
 			templateUrl: 'app/admin/admin.html',
+			data: {
+				requiresLogin : true
+			},
 			controller: 'AdminMainCtlr'
 		});
 });
