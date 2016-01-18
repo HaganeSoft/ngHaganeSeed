b44703dde0315dc31f0b7c795b11baef1ad59851
Erick Castillo de la Garza <ecastillo550@gmail.com>
2016-01-18 16:31:58 -0600

    configuraciones

--- app/app.run.js
+++ app/app.run.js
@@ -1,13 +1,29 @@
-app.run(['$rootScope', '$state', 'hagane', function ($rootScope, $state, hagane) {
+app.run(['$rootScope', '$state', 'hagane', 'HG_AUTH_EVENTS', function ($rootScope, $state, hagane, HG_AUTH_EVENTS) {
 	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
-	var isAuthenticationRequired =  toState.data
-		  && toState.data.requiresLogin
-		  && !hagane.session.authorize();
+		var isAuthenticationRequired =  toState.data
+		&& toState.data.loginRequired;
 
-	if(isAuthenticationRequired)
-	{
-	  event.preventDefault();
-	  $state.go('login');
-	}
+		if(isAuthenticationRequired)
+		{
+			var role = hagane.session.getRole();
+			if (role) {
+				if (toState.data.roles.indexOf(role) < 0) {
+					event.preventDefault();
+					$state.go('login');
+				}
+			} else {
+				hagane.session.authorize().then(function (res) {
+					if (toState.data.roles.indexOf(res.user.role) < 0) {
+						event.preventDefault();
+						$state.go('login');
+					} else {
+						$rootScope.$broadcast(HG_AUTH_EVENTS.IS_AUTHORIZED);
+					}
+				}, function (res) {
+					event.preventDefault();
+					$state.go('login');
+				});
+			}
+		}
 	});
 }]);
